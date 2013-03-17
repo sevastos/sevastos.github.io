26cc38812a2c0fd2ca1164cd8e0144d62a2357b6
Sev <sevastos.mastrogiorgis@gmail.com>
2013-03-17 09:13:14 +0200

    Publish shell script

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#currBranch=`git branch --no-color | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+currBranch=`git rev-parse --abbrev-ref HEAD`
+
+# Publish source
+git push -n public $currBranch:source
+
+# Generate docpad files
+docpad generate --env static
+
+# Publish generated files
+git commit -m "Generated"
+git push -n public $currBranch:master
