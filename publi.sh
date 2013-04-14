061fc60ae76ce4a79b86f5e338e60bd4ce464613
sevastos <me@sevthedev.com>
2013-04-14 09:57:48 +0300

    Publish script changes

--- publi.sh
+++ publi.sh
@@ -8,10 +8,24 @@ git push -n public $currBranch:source
 
 # Generate docpad files
 docpad generate --env static
+cp CNAME out/CNAME
+cp README out/README
+cd out
+git init
+git remote add public git@github.com:sevastos/sevastos.github.com.git
 
 # Publish generated files
-git commit -m "Generated"
-git push -n public $currBranch:master
+git add .
+echo "Type the commit message of the publish repo: "
+read commit_msg
+if [[ -z "${commit_msg}" ]]; then
+	commit_msg = "Generated"
+fi
+git commit -m "Pub: $commit_msg" # Concat unpublish commit messages OR ask user
+git push -n public master:master
 
 # Erase last commit (Docpad Generation)
-git reset --hard HEAD~1
+#git reset --hard HEAD~1
+
+cd ..
+#rm -rf out/*
